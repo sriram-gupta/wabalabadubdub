78e6fe546a39c0ca271e24f26976911ab49552b1
jarvis <jarvis@jarviss-MacBook-Pro.local>
2023-09-10 17:29:23 +0530

    added home path

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/")
+def HealthCheck():
+    return "<h1>Welcome Home ! (C) SRK </h1>"
+
+
 @app.get("/health")
 def HealthCheck():
     return { "status": "ok", "msg": "Health Check Working Properly" }
