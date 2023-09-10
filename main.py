5ca633f59c91bd3a612f10f33f1d1c204b043ce8
jarvis <jarvis@jarviss-MacBook-Pro.local>
2023-09-10 18:09:59 +0530

    added html path

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse)
 def HealthCheck():
     return "<h1>Welcome Home ! (C) SRK </h1>"
 
